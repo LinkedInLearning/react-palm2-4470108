985d2039857f9f4205dc006ab4135cd6befa348b
Ray Villalobos <ray@planetoftheweb.com>
2023-08-02 05:00:01 -0400

    01_06

--- palm-server/index.js
+++ palm-server/index.js
@@ -1,4 +1,7 @@
 require('dotenv').config();
+const express = require('express');
+const app = express();
+
 const { TextServiceClient } =
   require("@google-ai/generativelanguage").v1beta2;
 
@@ -11,15 +14,24 @@ const client = new TextServiceClient({
   authClient: new GoogleAuth().fromAPIKey(API_KEY),
 });
 
+let answer = null;
 const prompt = "Repeat after me: one, two,";
 
-client
-  .generateText({
-    model: MODEL_NAME,
-    prompt: {
-      text: prompt,
-    },
-  })
-  .then((result) => {
-    console.log(JSON.stringify(result));
-  });
+app.get('/api', (req, res) => {
+  client
+    .generateText({
+      model: MODEL_NAME,
+      prompt: {
+        text: prompt,
+      },
+    })
+    .then((result) => {
+      answer = result[0].candidates[0].output;
+      res.json(answer)
+    }).catch((err) => {
+      console.error(err.details);
+      res.json(err.details);
+    });
+});
+
+app.listen(3333, () => console.log('Server running on port 3333'));
