0003337503bad5e75dcb5097e98ae79a043d64ff
Ray Villalobos <ray@planetoftheweb.com>
2023-08-02 08:06:21 -0400

    added palm-client

---
+++ palm-client/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <main>
+      <h1>MyPrompter</h1>
+      <article />
+      <div>
+        <textarea />
+        <button>Go</button>
+      </div>
+    </main>
+  )
+}
+
+export default App
