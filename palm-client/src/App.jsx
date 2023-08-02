38cecec782729b66e387e98aa9a4dfcf0af7a4bd
Ray Villalobos <ray@planetoftheweb.com>
2023-08-02 16:23:46 -0400

    TextArea Sends Information

--- palm-client/src/App.jsx
+++ palm-client/src/App.jsx
@@ -2,8 +2,8 @@ import { useState, useEffect } from 'react'
 
 function App() {
   const [serverData, setServerData] = useState('')
+  const [userPrompt, setUserPrompt] = useState('')
   function handleSubmit() {
-    const userPrompt = 'The Quick Brown Fox'
     fetch('/api', {
       method: 'POST',
       headers: {
@@ -21,7 +21,7 @@ function App() {
   return (
     <main style={{ height: '100vh', display: 'flex', flexDirection: 'column' }}>
       <h1 style={{ padding: '10px', marginBottom: '0' }}>MyPrompter</h1>
-      <div style={{ margin: '0', flexGrow: '1' }}>
+      <div style={{ margin: '0', flexGrow: '1', overflow: 'scroll' }}>
         <div style={{ width: '100%', height: '100%' }}>
           <article style={{ margin: '0' }}>
             {serverData}
@@ -29,7 +29,7 @@ function App() {
         </div>
       </div>
       <div style={{ display: 'flex', alignItems: 'end', backgroundColor: '#222', padding: '10px' }}>
-        <textarea style={{ margin: '0', flexGrow: '1', overflowY: 'hidden' }} placeholder='Type in Prompt...' />
+        <textarea onChange={(e) => setUserPrompt(e.target.value)} style={{ margin: '0', flexGrow: '1', overflowY: 'hidden' }} placeholder='Type in Prompt...' />
         <button onClick={handleSubmit} style={{ margin: '0', flex: '1', marginLeft: '10px' }}>Go</button>
       </div>
     </main>
