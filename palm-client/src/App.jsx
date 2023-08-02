0b58d060707faae5043ac48be07231bdf2526cc6
Ray Villalobos <ray@planetoftheweb.com>
2023-08-02 09:54:06 -0400

    02_02

--- palm-client/src/App.jsx
+++ palm-client/src/App.jsx
@@ -3,12 +3,16 @@ import { useState } from 'react'
 function App() {
 
   return (
-    <main>
-      <h1>MyPrompter</h1>
-      <article />
-      <div>
-        <textarea />
-        <button>Go</button>
+    <main style={{ height: '100vh', display: 'flex', flexDirection: 'column' }}>
+      <h1 style={{ padding: '10px', marginBottom: '0' }}>MyPrompter</h1>
+      <div style={{ margin: '0', flexGrow: '1' }}>
+        <div style={{ width: '100%', height: '100%' }}>
+          <article style={{ margin: '0' }} />
+        </div>
+      </div>
+      <div style={{ display: 'flex', alignItems: 'end', backgroundColor: '#222', padding: '10px' }}>
+        <textarea style={{ margin: '0', flexGrow: '1', overflowY: 'hidden' }} placeholder='Type in Prompt...' />
+        <button style={{ margin: '0', flex: '1', marginLeft: '10px' }}>Go</button>
       </div>
     </main>
   )
